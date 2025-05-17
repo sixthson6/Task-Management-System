0beec7e6a641929b61643bb8d014cb478e991690
sixthson6 <sudais2408@gmail.com>
2025-05-16 22:03:34 -0700

    Add task model class

---
+++ src/main/java/com/novatech/model/Task.java
@@ -0,0 +1,54 @@
+package com.novatech.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Task {
+    private int id;
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private Status status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Task() { }
+
+    public Task(String title, String description, LocalDate dueDate, Status status) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.status = status;
+    }
+
+    public Task(int id, String title, String description, LocalDate dueDate, Status status, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public LocalDate getDueDate() { return dueDate; }
+    public void setDueDate(LocalDate dueDate) { this.dueDate = dueDate; }
+
+    public Status getStatus() { return status; }
+    public void setStatus(Status status) { this.status = status; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+
+    public LocalDateTime getUpdatedAt() { return updatedAt; }
+    public void setUpdatedAt(LocalDateTime updatedAt) { this.updatedAt = updatedAt; }
+}
